ebd3dbf13de19ef41afbb1b4c1689f5d0c04f611
Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>
2019-05-01 23:47:05 -0300

    Implementada task de conexão http por requisição post

--- main/dalton_tasks.c
+++ main/dalton_tasks.c
@@ -77,11 +77,13 @@ void dalton_color_task(void *pvParameter)
 				hex_code = color_st.hex_code;
 			}
 
-			if (counter_range >= 85){
+			if (counter_range >= 185){
 				printf("HSV: h = %.2f , s = %.2f, v = %.2f \n", hsv.h, hsv.s, hsv.v);
 				printf("counter: %i\n", counter_range);
 				printf("Cor detectada: %s\n\n", color_st.name);
 				*color_to_diplay_st = color_st;
+				//Notifica a task de Http somente apos uma leitura concluida...
+				xTaskNotify(xTaskHandlerHttp,0x00,eNoAction);
 			}
 			counter_range = 0;
 			vTaskDelay(100 / portTICK_RATE_MS);
@@ -92,117 +94,21 @@ void dalton_color_task(void *pvParameter)
 	vTaskDelete(NULL);
 }
 
-void dalton_http_get_task(void *pvParameters)
-{
-    const struct addrinfo hints = {
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_STREAM,
-    };
-    struct addrinfo *res;
-    struct in_addr *addr;
-    int s, r;
-    char recv_buf[64];
-
-    while(1) {
-        /* Wait for the callback to set the CONNECTED_BIT in the
-           event group.
-        */
-        xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT_WIFI,
-                            false, true, portMAX_DELAY);
-        ESP_LOGI(TAG_HTTP, "Connected to AP");
-
-        int err = getaddrinfo(WEB_SERVER, "80", &hints, &res);
-
-        if(err != 0 || res == NULL) {
-            ESP_LOGE(TAG_HTTP, "DNS lookup failed err=%d res=%p", err, res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        /* Code to print the resolved IP.
-
-           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
-        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-        ESP_LOGI(TAG_HTTP, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
-
-        s = socket(res->ai_family, res->ai_socktype, 0);
-        if(s < 0) {
-            ESP_LOGE(TAG_HTTP, "... Failed to allocate socket.");
-            freeaddrinfo(res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG_HTTP, "... allocated socket");
-
-        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
-            ESP_LOGE(TAG_HTTP, "... socket connect failed errno=%d", errno);
-            close(s);
-            freeaddrinfo(res);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        ESP_LOGI(TAG_HTTP, "... connected");
-        freeaddrinfo(res);
-
-        if (write(s, REQUEST_HTTP, strlen(REQUEST_HTTP)) < 0) {
-            ESP_LOGE(TAG_HTTP, "... socket send failed");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG_HTTP, "... socket send success");
-
-        struct timeval receiving_timeout;
-        receiving_timeout.tv_sec = 5;
-        receiving_timeout.tv_usec = 0;
-        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
-                sizeof(receiving_timeout)) < 0) {
-            ESP_LOGE(TAG_HTTP, "... failed to set socket receiving timeout");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG_HTTP, "... set socket receiving timeout success");
-
-        /* Read HTTP response */
-        do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            for(int i = 0; i < r; i++) {
-                putchar(recv_buf[i]);
-            }
-        } while(r > 0);
-
-        ESP_LOGI(TAG_HTTP, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
-        close(s);
-        for(int countdown = 10; countdown >= 0; countdown--) {
-            ESP_LOGI(TAG_HTTP, "%d... ", countdown);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        ESP_LOGI(TAG_HTTP, "Starting again!");
-    }
-}
-
 void dalton_http_test_task(void *pvParameters)
 {
-    app_wifi_wait_connected();
-    ESP_LOGI(TAG_HTTP_CLIENT, "Connected to AP, begin http example");
-    http_rest_with_url();
-    http_rest_with_hostname_path();
-    http_auth_basic();
-    http_auth_basic_redirect();
-    http_auth_digest();
-    http_relative_redirect();
-    http_absolute_redirect();
-    https_with_url();
-    https_with_hostname_path();
-    http_redirect_to_https();
-    http_download_chunk();
-    http_perform_as_stream_reader();
-    https_async();
+	_dalton_wifi_wait_connected();
+	ESP_LOGI(TAG_HTTP_CLIENT, "Connected to Acess Point...");
 
-    ESP_LOGI(TAG_HTTP_CLIENT, "Finish http example");
-    vTaskDelete(NULL);
+	while (1){
+		if (pdTRUE == xTaskNotifyWait(0x00, ULONG_MAX, 0x00, portMAX_DELAY))
+		{
+			_dalton_wifi_wait_connected();
+			ESP_LOGI(TAG_HTTP_CLIENT, "Connected to Acess Point...");
+			ESP_LOGI(TAG_HTTP_CLIENT, "HTTP POST id color = %s", color_to_diplay_st->id);
+			_dalton_http_post(color_to_diplay_st);
+		}
+	}
+	ESP_LOGI(TAG_HTTP_CLIENT, "Finish http");
+	vTaskDelete(NULL);
 }
 
